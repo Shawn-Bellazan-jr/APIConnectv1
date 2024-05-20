39eef889420c4a320c6ba0574d41aefc96216c64
Shawn-Bellazan-jr <166971628+Shawn-Bellazan-jr@users.noreply.github.com>
2024-05-20 16:25:20 -0500

    refactor(api-connect): Rename BaseClasses directory to Abstract
    Renames the BaseClasses directory to Abstract to better reflect its purpose and maintain consistency with naming conventions.

---
+++ src/APIConnect.Core/AuthenticationHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIConnect.Core.Abstractions
+{
+    internal interface AuthenticationHelper
+    {
+    }
+}
