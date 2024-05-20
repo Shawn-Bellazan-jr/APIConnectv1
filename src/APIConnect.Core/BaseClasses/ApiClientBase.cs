4521c06b73f59e80bfcab9ba7b577ac9c1f3dfff
Shawn-Bellazan-jr <166971628+Shawn-Bellazan-jr@users.noreply.github.com>
2024-05-20 16:15:10 -0500

    feat(api-connect): Create BaseClasses directory with ApiClientBase abstract class
    Adds a new directory called BaseClasses within the APIConnect.Core project. This directory contains the ApiClientBase abstract class, which serves as a foundation for all API clients within the application.

---
+++ src/APIConnect.Core/BaseClasses/ApiClientBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIConnect.Core.BaseClasses
+{
+    internal class ApiClientBase
+    {
+    }
+}
